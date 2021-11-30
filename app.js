f3b28b3e9fbbc2f0ee6522aeb6f9547981422d0c
Kealoha1988 <kealohaabe1988@gmail.com>
2021-11-29 22:50:36 -0500

    created Class BTS with insert method

--- app.js
+++ app.js
@@ -6,6 +6,43 @@ class Node {
   }
 }
 
+class BTS {
+  constructor(value) {
+    this.root = new Node(value)
+    this.count = 0
+  }
+ size() {
+  this.count
+ }
+
+ insert() {
+  this.count++
+  let newNode = new Node(value)
+  const searchTree = (node) => {
+    if(value < node.value){
+      if (!node.left) {
+        node.left = newNode
+      }else{
+        searchTree(node.left)
+      }
+    }
+    else if (value > node.value){
+      if(!node.right){
+        node.right = newNode
+      }else{
+        searchTree(node.right)
+      }
+    }
+  }
+  searchTree(this.root)
+ }
+
+
+
+
+
+}
+
 
 
 
